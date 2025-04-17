7119ab9255e27239c006c5918d5734aac4300117
adenugroho31 <adenugroho31122004@gmail.com>
2025-04-17 22:02:03 +0700

    Autentifikasi Selesai

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
+import RegisterStepOne from "../pages/RegisterStepOne";
+import RegisterStepTwo from "../pages/RegisterStepTwo";
+import ForgotPassword from '../pages/ForgotPassword';
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-
+        <Route path="/login" element={<Login />} />
+        <Route path="/register-step-one" element={<RegisterStepOne />} />
+        <Route path="/register-step-two" element={<RegisterStepTwo />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </BrowserRouter>
   );
