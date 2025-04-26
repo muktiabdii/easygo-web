a8f587a88b53f3064d1c3865111b76e8e66e21c1
muktiabdii <abdisyukur10@gmail.com>
2025-04-26 10:55:09 +0700

    install pnpm dan revisi router

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -7,8 +7,6 @@ import ForgotPasswordStepOne from '../pages/ForgotPasswordStepOne'
 import ForgotPasswordStepTwo from '../pages/ForgotPasswordStepTwo'
 import Dashboard from '../pages/Dashboard'
 import ChatPage from '../pages/ChatPage'
-import ForgotPassword from '../pages/ForgotPassword';
-import Dashboard from '../pages/Dashboard';
 import Maps from "../pages/Maps";
 import AddPlace from "../pages/AddPlace";
 
@@ -20,11 +18,10 @@ export default function AppRouter() {
         <Route path="/login" element={<Login />} />
         <Route path="/register-step-one" element={<RegisterStepOne />} />
         <Route path="/register-step-two" element={<RegisterStepTwo />} />
-        <Route path="/forgot-password" element={<ForgotPasswordStepOne />} />
+        <Route path="/forgot-password-step-one" element={<ForgotPasswordStepOne />} />
         <Route path="/forgot-password-step-two" element={<ForgotPasswordStepTwo />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/chat" element={<ChatPage />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/maps" element={<Maps />} />
         <Route path="/tambah-tempat" element={<AddPlace />} />
