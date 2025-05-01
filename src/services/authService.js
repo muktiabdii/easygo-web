cc6beb690dea2c194bbbb0016bae409a87080a28
Dzvelocity <dzikrimurtadlo@gmail.com>
2025-05-01 17:02:09 +0700

    resolve conflict

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 // inisiasi API URL
 const Login_API_URL = 'http://localhost:8000/api/auth/login'; 
 const Register_API_URL = 'http://localhost:8000/api/auth/register';
+const ForgotPassword_API_URL = 'http://localhost:8000/api/auth/password/forgot';
+const validateOTP_API_URL = 'http://localhost:8000/api/auth/password/validate-otp';
+const ResetPassword_API_URL = 'http://localhost:8000/api/auth/password/reset';
 
 // fungsi untuk login
 export const login = async (email, password) => {
@@ -24,6 +27,42 @@ export const register = async (data) => {
   } 
   
   catch (error) {
-    throw error.response ? error.response.data : error.message; // Menangani error
+    throw error.response ? error.response.data : error.message; 
+  }
+};
+
+// fungsi untuk mengirim email untuk reset password
+export const forgotPassword = async (email) => {
+  try {
+    const response = await axios.post(ForgotPassword_API_URL, { email });
+    return response.data; 
+  } 
+  
+  catch (error) {
+    throw error.response ? error.response.data : error.message; 
+  }
+};
+
+// fungsi untuk validasi OTP
+export const validateOTP = async (email, otp) => {
+  try {
+    const response = await axios.post(validateOTP_API_URL, { email, otp });
+    return response.data; 
+  } 
+  
+  catch (error) {
+    throw error.response ? error.response.data : error.message; 
+  }
+};
+
+// fungsi untuk reset password
+export const resetPassword = async (email, otp, password, password_confirmation) => {
+  try {
+    const response = await axios.post(ResetPassword_API_URL, { email, otp, password, password_confirmation });
+    return response.data; 
+  } 
+  
+  catch (error) {
+    throw error.response ? error.response.data : error.message; 
   }
 };
